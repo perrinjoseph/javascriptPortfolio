c18c4eddc3a599c8f19c5fceb870d1a80730ed1d
perrinjoseph <perrinjoseph1998@gmail.com>
2021-02-07 00:17:19 -0500

    wokred on bankist Dom stuff

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -7,7 +7,7 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-
+const header = document.querySelector('header');
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -32,6 +32,7 @@ document.addEventListener('keydown', function (e) {
 });
 
 /* ------------------------------ PRACTICE CODE ----------------------------- */
+
 console.log(document.documentElement);
 console.log(document.body);
 const somenodes = document.querySelectorAll('.section');
@@ -40,3 +41,30 @@ console.log(all);
 console.log(somenodes);
 const classElemets = document.getElementsByClassName('btn');
 console.log(classElemets);
+const navItems = document.querySelectorAll('.nav__item');
+console.log(navItems);
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent= `We use cookies for improves functionality and analytics.`;
+message.innerHTML = 'We used Cookig to improve functionality and analytics. <button class="btn btn--close-cookie">Got it! </button>';
+
+//prepending add the newly craeted element as the first child of the parent element that is being refered too. In this case 'header is the parent element'
+
+header.prepend(message);
+
+// //the reason why the dom did not show both the prepend and the append is because this is a live htmlcollection, and live elements cannot exist in different places at the sme time. In order to do this you fisr have to copy the element. using the cloneNode(true);
+
+// header.append(message.cloneNode(true));
+// header.append(message);
+// header.before(message);
+// header.after(message);
+// header.classList.remove('btn btn--close-cookie');
+//deleting elements
+
+document.querySelector('.btn--close-cookie').addEventListener('click',function(){
+  message.remove();
+});
+
+
+
