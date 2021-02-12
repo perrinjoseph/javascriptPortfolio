ace8a876901bed5087a2abb6de6b23a61a631e38
Emmanuel Perrin Joseph <ejoseph3@masonlive.gmu.edu>
2021-02-12 05:00:00 -0500

    Object oriented skills and Map Program

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -20,7 +20,7 @@ if(navigator.geolocation)
             //38.870032200000004 -77.277886
             console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-            var map = L.map('map').setView([51.505, -0.09], 13);
+            const map = L.map('map').setView([51.505, -0.09], 13);
 
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
